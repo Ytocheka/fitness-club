58948f87bb8c51ac5e7480fee2a2339cd047c66a
yto <ytocheeka@gmail.com>
2026-05-13 09:33:00 +0300

    Fix settings order

--- fitness_club/settings.py
+++ fitness_club/settings.py
@@ -126,6 +126,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = [
+    MEDIA_ROOT,
+]
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Custom user model
